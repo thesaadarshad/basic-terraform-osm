73fe70ca40a2431873625e89748c69a989f22276
Ubuntu <ubuntu@ip-172-31-16-46.eu-west-1.compute.internal>
2019-01-31 07:31:42 +0000

    Initial commit

---
+++ main.tf
@@ -0,0 +1,18 @@
+provider "aws" {
+    region = "eu-west-1"
+}
+
+
+resource "aws_instance" "web_server"{
+  count                                = "1"
+  ami                                  = "ami-049f47f8a332ba288"
+  instance_type                        = "t2.micro"
+  key_name                             = "webserver_key"
+  vpc_security_group_ids               = ["sg-0ecda5c095519f32f"]
+  tags                                 { Name = "web-server" }
+}
+
+output "public_ip" {
+  value = "${aws_instance.web_server.public_ip}"
+}
+
